edcd034b9e7626376fb6818e341c2be0875663ad
Nihal <75980873+Nihal-sketch@users.noreply.github.com>
2022-07-14 12:17:30 +0530

    Add files via upload

---
+++ output(8).c
@@ -0,0 +1,22 @@
+Enter the number of processes and resources 
+5 3
+
+Enter the allocation matrix:
+0 1 0
+2 0 0
+3 2 0 
+0 0 2
+
+2 Enter the maximum mamix:
+ 7 5 3 
+ 3 2 2 
+ 9 0 2
+ 2 2 2
+ 4 3 3
+
+the avaible array: 3 3 2
+.
+.
+.
+Additional Requests: 1 0 2
+enter number of process:1
